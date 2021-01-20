d82c6b2b64330fa4b38132b1b0316c0139f48b38
Alexis Ordenes <giraalexis@outlook.com>
2021-01-20 17:43:09 -0300

    Agregar Funcion LogOut

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
-        Chat component
+        <button @click="logout" class="btn btn-warning float-right">Logout</button>
     </div>
 </template>
+
+<script>
+import firebase from 'firebase'
+export default {
+    methods: {
+        logout(){
+            firebase.auth().signOut()
+        }
+    }
+}
+</script>
